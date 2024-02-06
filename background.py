968dae6413e6547a1c335ef80cbe8a723bc7437a
quentinmel <qmeleo@gmail.com>
2024-02-06 22:55:54 +0100

    add assets

--- background.py
+++ background.py
@@ -6,3 +6,4 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+        
