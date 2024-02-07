906d966a2db925cf5d7b5fdfbeeeb34fb2512e16
quentinmel <qmeleo@gmail.com>
2024-02-07 19:31:14 +0100

    move player class in player.py

--- game.py
+++ game.py
@@ -1,41 +1,9 @@
-import sys
 import pygame
+from player import Player
 
 width = 1280
 height = 720
-
-class Player:
-    def __init__(self, x, y, radius, color):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.velocity_keyboard = 5
-        self.velocity_mouse = 2
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
-
-    def move(self, keys, mouse_pos, mode):
-        if mode == "Keyboard":
-            if keys[pygame.K_q]:
-                self.x -= self.velocity_keyboard
-            if keys[pygame.K_d]:
-                self.x += self.velocity_keyboard
-            if keys[pygame.K_z]:
-                self.y -= self.velocity_keyboard
-            if keys[pygame.K_s]:
-                self.y += self.velocity_keyboard
-        elif mode == "Mouse":
-            target_x, target_y = mouse_pos
-            dx = target_x - self.x
-            dy = target_y - self.y
-            distance = max(abs(dx), abs(dy))
-            if distance != 0:
-                self.x += dx / distance * self.velocity_mouse
-                self.y += dy / distance * self.velocity_mouse
     
-
 clock = pygame.time.Clock()
 
 player = Player(50, 50, 25, (255, 0, 0))
@@ -51,13 +19,13 @@ def PlayGame(mode):
         
         mouse_pos = pygame.mouse.get_pos()
         keys = pygame.key.get_pressed()
+        
         if keys[pygame.K_ESCAPE]:
                     new_game_started = False
                     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()
                 
             player.move(keys, mouse_pos, mode)
         
