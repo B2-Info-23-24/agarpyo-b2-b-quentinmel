e5275f4383d8bc473499cd106e082e8b32b3064e
quentinmel <qmeleo@gmail.com>
2024-02-09 23:28:28 +0100

    update: put title and size screen

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from menu import main_menu
+import pygame
+from menu import Menu
+
+def main():
+    pygame.init()
+
+    width = 1280
+    height = 720
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Agarpyo")
+
+    menu = Menu(screen)
+    menu.run()
+
+    pygame.quit()
 
 if __name__ == '__main__':
-    main_menu()
+    main()
