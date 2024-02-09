3c8c6c75b4141342fea116d2a79fd1e6521638c0
quentinmel <qmeleo@gmail.com>
2024-02-09 23:27:54 +0100

    create class Game

--- game.py
+++ game.py
@@ -1,38 +1,53 @@
 import pygame
 from player import Player
 from obstacle import Obstacle
+from food import Food
 
-width = 1280
-height = 720
+class Game:
+    def __init__(self, screen, mode, difficulty):
+        pygame.init()
+        self.screen = screen
+        self.mode = mode
+        self.difficulty = difficulty
+        self.width = 1280
+        self.height = 720
+        self.clock = pygame.time.Clock()
+        food_rects = []
+        self.obstacle = Obstacle(0, 0, 0, food_rects)
+        self.obstacle.createObstacle(self.difficulty, food_rects)
+        obstacle_rects = self.obstacle.get_obstacle_rects()
+        self.food = Food(0, 0, 0, obstacle_rects)
+        self.food.createFood(self.difficulty, obstacle_rects)
+        food_rects = self.food.get_food_rects()
+        self.player = Player(50, 50, 25, (255, 0, 0), obstacle_rects, food_rects)
 
-clock = pygame.time.Clock()
+    def run(self):
+        pygame.display.set_mode((self.width, self.height))
+        new_game_started = True
+        FPS = 60
 
-player = Player(50, 50, 25, (255, 0, 0))
-obstacle = Obstacle(200, 200, 50)
+        while new_game_started:
+            self.screen.fill((255, 255, 255))
+            self.player.draw(self.screen)
+            for obs in self.obstacle.obstacles:
+                obs.draw(self.screen)
+            for food in self.food.foods:
+                food.draw(self.screen)
+            pygame.display.update()
 
-def PlayGame(mode):
-    new_screen = pygame.display.set_mode((width, height))
-    new_game_started = True
-    
-    while new_game_started:
-        new_screen.fill((255, 255, 255))
-        player.draw(new_screen)
-        obstacle.draw(new_screen)
-        pygame.display.update()
-        
-        mouse_pos = pygame.mouse.get_pos()
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_ESCAPE]:
-            new_game_started = False
-            
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        
-        if obstacle.collide(player):
-            print("Collision")
-        
-        player.move(keys, mouse_pos, mode)
-        
-        pygame.display.update()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                new_game_started = False
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            if self.obstacle.collide(self.player, self.food.foods):
+                print("Collision Obstacle")
+            if self.food.collide(self.player, self.obstacle.obstacles):
+                print("Food Collision")
+
+            self.player.move(keys, pygame.mouse.get_pos(), self.mode, fps=FPS)
+            self.clock.tick(FPS)
+            pygame.display.update()
